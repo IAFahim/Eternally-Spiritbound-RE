274824eca10d614049b40662fb1de21d398e8b70
Md. Ishtiaq Ahamed Fahim Sky Tower <ishtiaq.fahim@northsouth.edu>
2024-03-17 02:29:07 +0600

    Added new Input System

--- Assets/_Root/Scripts/Controllers/TopDownController2D.cs
+++ Assets/_Root/Scripts/Controllers/TopDownController2D.cs
@@ -1,31 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace _Root.Scripts.Controllers
 {
     [RequireComponent(typeof(Rigidbody2D))]
-    public class TopDownController2D : TopDownController
+    public sealed class TopDownController2D : TopDownController
     {
-        public Rigidbody2D _rigidBody;
-        public Vector2 direction;
-        protected Vector3 _impact;
-        protected MovingPlatform2D _movingPlatform;
-        [Tooltip("the current added force, to be added to the character's movement")]
+        public Rigidbody2D rigidBody;
+        private Vector3 _impact;
+        private MovingPlatform2D _movingPlatform;
         public Vector3 AddedForce;
         
-        ///  whether or not the character is on a moving platform
         public override bool OnAMovingPlatform { get { return _movingPlatform; } }
 
         private void OnValidate()
         {
-            _rigidBody ??= GetComponent<Rigidbody2D>();
+            rigidBody ??= GetComponent<Rigidbody2D>();
         }
 
-        private void Update()
-        {
-            // CurrentMovement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        }
-
-        protected void FixedUpdate()
+        private void FixedUpdate()
         {
 
             ApplyImpact();
@@ -46,23 +39,23 @@ namespace _Root.Scripts.Controllers
                 CurrentMovement = Vector3.Lerp(Speed, CurrentMovement, Time.deltaTime * Friction);
             }
             
-            Vector2 newMovement = _rigidBody.position + (Vector2)(CurrentMovement + AddedForce) * Time.fixedDeltaTime;
+            Vector2 newMovement = rigidBody.position + (Vector2)(CurrentMovement + AddedForce) * Time.fixedDeltaTime;
             
             if (OnAMovingPlatform)
             {
                 newMovement += (Vector2)_movingPlatform.CurrentSpeed * Time.fixedDeltaTime;
             }
-            _rigidBody.MovePosition(newMovement);
+            rigidBody.MovePosition(newMovement);
         }
         
         /// <summary>
         /// Applies the current impact
         /// </summary>
-        protected virtual void ApplyImpact()
+        private void ApplyImpact()
         {
             if (_impact.magnitude > 0.2f)
             {
-                _rigidBody.AddForce(_impact);
+                rigidBody.AddForce(_impact);
             }
             _impact = Vector3.Lerp(_impact, Vector3.zero, 5f * Time.deltaTime);
         }
