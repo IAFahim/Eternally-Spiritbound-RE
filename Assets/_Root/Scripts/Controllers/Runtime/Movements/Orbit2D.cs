f37b1d72725a158ac85b89a76adf0c521beaf7b1
Md. Ishtiaq Ahamed Fahim Sky Tower <ishtiaq.fahim@northsouth.edu>
2024-05-10 21:10:07 +0600

    Working on RB Orbit

--- Assets/_Root/Scripts/Controllers/Runtime/Movements/Orbit2D.cs
+++ Assets/_Root/Scripts/Controllers/Runtime/Movements/Orbit2D.cs
@@ -1,20 +1,20 @@
-using System;
 using _Root.Scripts.Datas.Runtime;
 using _Root.Scripts.Datas.Runtime.Interfaces;
 using Pancake;
 using Pancake.Apex;
 using UnityEngine;
+using UnityEngine.Serialization;
 
-namespace _Root.Scripts.Controllers.Runtime
+namespace _Root.Scripts.Controllers.Runtime.Movements
 {
-    public class EllipticalPathRotation : GameComponent, IRange, IAngularSpeed, ILoop
+    public class Orbit2D : GameComponent, IRange, IAngularSpeed, ILoop
     {
         [field: SerializeField] public float Speed { get; set; } = 5;
         [field: SerializeField] public float Radius { get; set; } = 1;
         [field: SerializeField] public int LoopCount { get; set; } = 1;
 
         public float startAngle;
-        public bool rotate;
+        [FormerlySerializedAs("rotate")] public bool selfRotate;
 
         public Transform children;
         public Vector2Constant worldScale;
@@ -41,21 +41,25 @@ namespace _Root.Scripts.Controllers.Runtime
             }
             Modify();
         }
-
-        private void Modify()
-        {
-            var (sinAngle, cosAngle) = GetSinCos(_angle);
-            if (rotate) Rotate(sinAngle, cosAngle);
-            children.localPosition = new Vector3(Range.x * cosAngle, Range.y * sinAngle, 0f);
-        }
         
-
         private (float sinAngle, float cosAngle) GetSinCos(float theta)
         {
             return (Mathf.Sin(theta), Mathf.Cos(theta));
         }
 
-        private void Rotate(float sinAngle, float cosAngle)
+        private void Modify()
+        {
+            var (sinAngle, cosAngle) = GetSinCos(_angle);
+            if (selfRotate) SelfRotate(sinAngle, cosAngle);
+            Move(cosAngle, sinAngle);
+        }
+
+        protected virtual void Move(float cosAngle, float sinAngle)
+        {
+            children.localPosition = new Vector3(Range.x * cosAngle, Range.y * sinAngle, 0f);
+        }
+
+        protected virtual void SelfRotate(float sinAngle, float cosAngle)
         {
             children.rotation = Quaternion.AngleAxis(Mathf.Atan2(sinAngle, cosAngle) * Mathf.Rad2Deg, Vector3.forward);
         }
---
+++ Assets/_Root/Scripts/Controllers/Runtime/Movements/Physics/Orbit2DRigidBody.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace _Root.Scripts.Controllers.Runtime.Movements.Physics
+{
+    public class Orbit2DRigidBody: Orbit2D
+    {
+        public Rigidbody2D rb;
+
+    }
+}
